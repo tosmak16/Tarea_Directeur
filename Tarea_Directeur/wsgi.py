1fa0245c2a9a585d60ed8b7f372ba8a0d50cbda6
Andela Developer <tosmak16@gmail.com>
2018-06-22 09:20:57 +0100

    Django setup

---
+++ Tarea_Directeur/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Tarea_Directeur project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Tarea_Directeur.settings")
+
+application = get_wsgi_application()
