35c141c6dd35883ccb87da34bfaf1eefbcf931c5
Andela Developer <tosmak16@gmail.com>
2018-06-23 15:55:43 +0100

    add authentication - add signin route and views - add token base authentication

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from rest_framework import routers
-from .views import TareaViewSet
+from .views import TareaViewSet, UserAuthenticationView
 from django.conf.urls import url, include
 
 router = routers.DefaultRouter()
@@ -7,5 +7,7 @@ router = routers.DefaultRouter()
 router.register(r'tareas', TareaViewSet)
 
 urlpatterns = [
+    url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^', include(router.urls)),
+    url(r'^users/signin/', UserAuthenticationView.as_view())
 ]
--- api/views.py
+++ api/views.py
@@ -1,12 +1,32 @@
 # Create your views here.
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import Tarea
 from .serializers import TareaSerializer
+
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from  .permissions import IsOwner
 
 
 class TareaViewSet(viewsets.ModelViewSet):
 
     queryset = Tarea.objects.all()
     serializer_class = TareaSerializer
+    permission_classes = (permissions.IsAuthenticated, IsOwner)
 
     lookup_field = 'id'
+
+
+class UserAuthenticationView(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        if not serializer.is_valid():
+            return Response({'message': 'username and password is incorrect'})
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key, 'user_id': user.pk, 'email': user.email })
+
+
