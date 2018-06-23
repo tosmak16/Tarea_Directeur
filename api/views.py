969f7dbe35677abdf2fc9485b229e012b410a4b3
Andela Developer <tosmak16@gmail.com>
2018-06-23 12:40:23 +0100

    add model test - add view test - add views and model

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
 # Create your views here.
+from rest_framework import viewsets
+from .models import Tarea
+from .serializers import TareaSerializer
+
+class TareaViewSet(viewsets.ModelViewSet):
+
+    queryset = Tarea.objects.all()
+    serializer_class = TareaSerializer
+
+    lookup_field = 'id'
